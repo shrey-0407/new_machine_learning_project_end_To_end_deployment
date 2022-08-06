8d5347ac2c600437c99d6a9f1cffeba15e696677
shrey <shreyash966977@gmail.com>
2022-08-07 00:11:01 +0530

    housing folder created and with coding in logger and exception

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-
+from distutils.log import error
+import os,sys
+import logging
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "SHREYS NEW ML PROJECT"
+
+    try:
+        raise Exception("We are testing exception")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("WE ARE TESTING LOGGONG MODULE")
+        
+    return "CI CD PIPELINE HAS BEEN ESTABLISHED"
 
 
 if __name__=="__main__":
