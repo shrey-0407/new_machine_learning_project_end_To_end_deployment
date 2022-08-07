4e6e9f388aaa5c449467739c732f1d1a4ba14533
shrey <shreyash966977@gmail.com>
2022-08-07 15:35:38 +0530

    added templeted

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -7,7 +7,7 @@ from housing.util.util import read_yaml_file
 from housing.constant import *
 from housing.exception import HousingException
 
-class Configuration:
+class Configuartion:
     def __init__(self,
         config_file_path:str=CONFIG_FILE_PATH,
         current_time_stamp:str=CURRENT_TIME_STAMP) -> None:
@@ -17,12 +17,39 @@ class Configuration:
 
     def get_data_ingestion_config(self)->DataIngestionConfig:
         try:
-            self.config_info
-            dataset_download_url
-            tgz_download_dir
-            raw_data_dir
-            ingested_train_dir
-            ingested_test_dir
+            data_ingestion_artifact_dir=self.training_pipeline_config.artifact_dir
+            data_ingestion_artifact_dir=os.path.join(
+                artifact_dir,
+                DATA_INGESTION_ARTIFACT_DIR,
+                self.time_stamp
+            )
+
+
+            data_ingestion_info=self.config_info[DATA_INGESTION_CONFIG_KEY]
+
+            dataset_download_url=data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            tgz_download_dir=os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
+
+            )
+            raw_data_dir=os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
+
+            ingested_data_dir=os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+            )
+
+            ingested_train_dir=os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+            )
+            ingested_test_dir=os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+            )
 
 
 
