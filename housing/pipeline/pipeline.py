4e3e45cd2deac4b4a6638a3d8b7d73bbf1629c04
shrey <shreyash966977@gmail.com>
2022-08-07 20:05:19 +0530

    data ingestion coding

---
+++ housing/pipeline/pipeline.py
@@ -0,0 +1,6 @@
+from housing.config.configuration import Configuartion
+from housing.logger import logging, get_log_file_name
+from housing.exception import HousingException
+from housing.entity.artifact_entity import DataValidationArtifact
+from housing.component.data_ingestion import DataIngestion
+
